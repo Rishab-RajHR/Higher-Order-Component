b059a38b2a30b462bc97e1b89542a34be464ea46
Rishab Raj <rr0948555@gmail.com>
2025-12-31 05:40:56 +0530

    Higher Order Component

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import OnClick from './OnClick'
+import OnMouseOver from './OnMouseOver'
+
+const App = () => {
+  return (
+    <div>
+      <h2>This is React App.jsx file</h2>
+      <OnClick />
+      <OnMouseOver />
+    </div>
+  )
+}
+
+export default App
